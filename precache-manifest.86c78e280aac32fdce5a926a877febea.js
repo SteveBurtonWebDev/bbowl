4786f9b1a8690c04364e9d969d02464346814fe2
Steve Burton <steveburtonwebdev@gmail.com>
2019-10-15 17:32:01 +0100

    Updates

---
+++ precache-manifest.86c78e280aac32fdce5a926a877febea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c48283474a19d0ea59d8c72b609c622",
+    "url": "/bbowl/index.html"
+  },
+  {
+    "revision": "7f98c9f1b3e120f0f5af",
+    "url": "/bbowl/static/css/main.b994a612.chunk.css"
+  },
+  {
+    "revision": "f2a22632470b3aa46455",
+    "url": "/bbowl/static/js/2.63ea6beb.chunk.js"
+  },
+  {
+    "revision": "7f98c9f1b3e120f0f5af",
+    "url": "/bbowl/static/js/main.3882cd3e.chunk.js"
+  },
+  {
+    "revision": "4da7f389b5f37e9623e6",
+    "url": "/bbowl/static/js/runtime-main.240b9165.js"
+  },
+  {
+    "revision": "e93cc8781cd782be365e0643c798dfeb",
+    "url": "/bbowl/static/media/bb-bg.e93cc878.jpg"
+  },
+  {
+    "revision": "4c384a3e51540c687680684d42588acf",
+    "url": "/bbowl/static/media/croppedimage1201631-blood-bowl-2-background.4c384a3e.jpg"
+  },
+  {
+    "revision": "bd391b3d13122abdf8765c32aa003284",
+    "url": "/bbowl/static/media/gutcruncher-webfont.bd391b3d.woff2"
+  },
+  {
+    "revision": "c7315c53cdea0bf283623494b49ddce2",
+    "url": "/bbowl/static/media/gutcruncher-webfont.c7315c53.woff"
+  }
+]);
